c52f95ef4a999957185dd13e6ee9c47af605658e
Circle Liu <1176092962@qq.com>
2019-12-22 04:30:40 +0800

    etl

--- extract/review_trans.py
+++ extract/review_trans.py
@@ -6,12 +6,21 @@ number_of_reviews = 7911684
 with open('./rawdata/movies.txt', 'r', errors='ignore') as datafile:
     block = [''] * 9
     
-    with open('./csv/all_review.csv', 'w', newline='') as csvfile:
+    with open('./csv/all_review.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
 
         for n in tqdm(range(number_of_reviews)):
             for i in range(9):
                 block[i] = datafile.readline()
+                while i == 3 and not block[3].startswith('review/helpfulness'):
+                    block[3] = datafile.readline()
+                while i == 8 and block[8] != '\n':
+                    block[8] = datafile.readline()
 
-            row = [j.split(':', 1)[1].strip() if ':' in j else '' for j in block[:8]]
-            writer.writerow(row)
+            try:
+                row = [j.split(':', 1)[1].strip() for j in block[:8]]
+                writer.writerow(row)
+            except:
+                print(block)
+                break
+            
