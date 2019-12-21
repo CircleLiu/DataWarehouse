9cdf5e0ea0dbe2e389791e263415f08c75dabe0c
Circle <1176092962@qq.com>
2019-12-21 22:48:57 +0800

    Front End

---
+++ frontend/src/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import ElementUI from 'element-ui'
+import App from "./App.vue";
+import router from "./router";
+import "./plugins/element.js";
+
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
