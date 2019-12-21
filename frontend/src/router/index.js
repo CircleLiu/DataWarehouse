ac3955fa36e701543c8dd9b07256c8719a6e1407
Xian-Zhou <shige@tongji.edu.cn>
2019-12-22 03:20:38 +0800

    index页

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,18 +6,15 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Home
+    path: '/',
+    name: 'homeindex',
+    component: () => import('./views/index.vue'),
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  
+    path: '/graphdata',
+    name: 'graphdata',
+    component: () => import('./views/graph.vue'),
   }
 ];
 
---
+++ frontend/src/views/graph.vue
@@ -0,0 +1,56 @@
+<template>
+<el-container>
+        <el-header>
+            <el-menu
+            :default-active="activeIndex2"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+          
+            <el-menu-item index="/homeindex" @click="onclick0">AMAZON DATA</el-menu-item>
+            <el-menu-item index="/graphdata" @click="onclick1">GRAPH COMPUTATION</el-menu-item>
+            </el-menu>
+        </el-header>
+    <el-container>
+        <el-aside width="200px"></el-aside>
+
+        <el-main>
+       
+
+        </el-main>
+        <el-aside width="200px"></el-aside>
+    </el-container>
+</el-container>
+</template >
+
+<script>
+  export default {
+    methods: {
+    onclick0() {
+      this.$router.push({ path: "/homeindex" });
+    },
+     onclick1() {
+      this.$router.push({ path: "/graphdata" });
+    }
+  }
+  };
+</script>
+<style>
+  .el-header{
+    background-color: #333;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+</style>
